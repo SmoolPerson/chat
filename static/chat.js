4c805c533228d71875b0fd1f6eac285459103450
mario1234 <SmoolPerson@github.com>
2025-04-07 16:50:52 -0500

    websocket functionalities work now, fixed some other bugs

--- static/chat.js
+++ static/chat.js
@@ -1,9 +1,27 @@
 socket = new WebSocket("ws://127.0.0.1:5001");
+
+
+socket.addEventListener('open', function(event) {
+    socket.send("loadmsg");
+});
+
 socket.onmessage = function(event) {
-    console.log('Received message from server: ' + event.data);
+    const para = document.createElement("p");
+    let node;
+    if (event.data != 'Invalid cookie. Try logging in again') {
+    let message = JSON.parse(event.data);
+        node = document.createTextNode(message["username"] + ": " + message["message"]);
+    }
+    else {
+        node = document.createTextNode("An error occurred with authentication. Please try logging in again.");
+    }
+    para.appendChild(node);
+    const element = document.getElementById("chatdiv");
+    element.appendChild(para);
 };
+
 function send(){
-    authToken = document.cookie.split('=')[1]
-    jsonMessage = {"authToken": authToken, "message": document.getElementById("chatsend").value}
+    let authToken = document.cookie.split('=')[1]
+    let jsonMessage = {"authToken": authToken, "message": document.getElementById("chatsend").value}
     socket.send(JSON.stringify(jsonMessage));
 }
--- websocket.py
+++ websocket.py
@@ -3,14 +3,61 @@ import asyncio
 import json
 import sqlite3
 import time
+clientSet = set()
+
+def update_message_db(username, message):
+    conn = sqlite3.connect('maindb.db')
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO messageList (timestamp, username, message) VALUES (?, ?, ?)", (time.time(), username, message))
+    cursor.execute("SELECT * FROM messageList ORDER BY time ASC")
+    allitems = cursor.fetchall()
+    if len(allitems) > 50:
+        valuetoremove = allitems[0][1]
+        cursor.execute("DELETE FROM messageList WHERE username = ?", (valuetoremove,))
 
 async def handler(websocket):
     while True:
         conn = sqlite3.connect('maindb.db')
         cursor = conn.cursor()
-        message = await websocket.recv()
-        dictionary = json.loads(message)
-        print(dictionary)
+        clientSet.add(websocket)
+        receivedmessage = await websocket.recv()
+        if receivedmessage == "loadmsg":
+            print("client requesting for initial message load")
+            cursor.execute("SELECT * FROM messageList ORDER BY timestamp ASC")
+            fetchedmessages = cursor.fetchall()
+            sendobj = [dict(item[1], item[2]) for item in fetchedmessages]
+            print(json.dumps(sendobj))
+        else:            
+            dictionary = json.loads(receivedmessage)
+            message = dictionary.get("message")
+            authToken = dictionary.get("authToken")
+
+            cursor.execute("SELECT * FROM authTokenList WHERE authToken = ?", (authToken,))
+            fetched = cursor.fetchall()
+            conn.commit()
+            conn.close()
+
+            if (fetched == [] or authToken == None):
+                await websocket.send("Invalid cookie. Try logging in again")
+            else:
+                conn = sqlite3.connect('maindb.db')
+                cursor = conn.cursor()
+                username = fetched[0][1]
+
+                cursor.execute("INSERT INTO messageList (timestamp, username, message) VALUES (?, ?, ?)", (time.time(), username, message))
+                cursor.execute("SELECT * FROM messageList ORDER BY timestamp ASC")
+                allitems = cursor.fetchall()
+
+                if len(allitems) > 50:
+                    valuetoremove = allitems[0][0]
+                    cursor.execute("DELETE FROM messageList WHERE timestamp = ?", (valuetoremove,))
+
+                broadcastobj = {"username": username, "message": message}
+                websockets.broadcast(clientSet, json.dumps(broadcastobj))
+                cursor.execute("SELECT * FROM messageList")
+
+                conn.commit()
+                conn.close()
 
 
 async def main():
