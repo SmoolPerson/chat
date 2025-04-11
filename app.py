4476d7616a1059e08bc1a7331c4ddf78852dcf15
mario1234 <SmoolPerson@github.com>
2025-04-10 22:27:52 -0500

    more tweaks and tuning

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, make_response, redirect
 import sqlite3
 import secrets
+import hashlib
 
 connection = sqlite3.connect('maindb.db')
 cursor = connection.cursor()
+# create the main tables for the program
 cursor.execute("CREATE TABLE IF NOT EXISTS loginInfo ( username varchar(255), password varchar(255) )")
 cursor.execute("CREATE TABLE IF NOT EXISTS authTokenList ( authToken varchar(255), username varchar(255) )")
 cursor.execute("CREATE TABLE IF NOT EXISTS messageList ( timestamp FLOAT, username varchar(255), message varchar(255) )")
@@ -13,36 +15,50 @@ connection.close()
 
 app = Flask(__name__)
 
+# generate sha256 hash with hashlib
+def generate_sha256_hash(input_string):
+    encoded_string = input_string.encode('utf-8')
+    sha256_hash = hashlib.sha256(encoded_string)
+    hex_digest = sha256_hash.hexdigest()
+    return hex_digest
+
+# generates a request with a authentication cookie for the user
 def generate_cookie(username):
     conn = sqlite3.connect('maindb.db')
     cursor = conn.cursor()
 
     response = make_response(render_template('chat.html'), 200)
-    authToken = secrets.token_bytes(16).hex()
+    authToken = secrets.token_bytes(16).hex() # 16 random bytes are generated for the cookie
 
     response.set_cookie('authCookie', authToken)
-    cursor.execute("SELECT * FROM authTokenList WHERE username = ?", (username,))
+    # check if a user already exists on the cookie db
+    cursor.execute("SELECT * FROM authTokenList WHERE username = ?", (username,)) 
     fetched = cursor.fetchall()
     if (fetched == []):
+        # insert record into list
         cursor.execute("INSERT INTO authTokenList (authToken, username) VALUES (?, ?)", (authToken, username))
     else:
+        # update record in list
         cursor.execute("UPDATE authTokenList SET authToken = ? WHERE username = ?", (authToken, username))
     
     conn.commit()
     conn.close()
     return response
 
+# takes an authtoken and returns the username if it is, and None if it doesnt exist
 def authorize(authToken):
-    if authToken == None:
+    if authToken == None: # if the parameter is None, then obviously the username is also none
         return None
     conn = sqlite3.connect('maindb.db')
     cursor = conn.cursor()
+    # find username in authtoken
     cursor.execute("SELECT * FROM authTokenList WHERE authToken = ?", (authToken,))
     fetched = cursor.fetchall()
-    if fetched == []:
+    if fetched == []: # if no username exists, return none
         return None
     return fetched[0][1]
 
+# print debug info from db
 def debug_database():
     conn = sqlite3.connect('maindb.db')
     cursor = conn.cursor()
@@ -57,28 +73,33 @@ def debug_database():
     conn.commit()
     conn.close()
 
+# display login page and autologin user
 @app.route("/")
 def index():
+    # authorize, auto redirect to chat.html if cookie is valid
     username = authorize(request.cookies.get("authCookie"))
     if username == None:
         return render_template('login.html', auth='')
     else:
         return render_template('chat.html')
     
-@app.route("/login", methods=["POST"])
+
+# handle logic for logging in
+@app.route("/chat", methods=["POST"])
 def login():
     conn = sqlite3.connect('maindb.db')
     cursor = conn.cursor()
 
     username = request.form['username']
     password = request.form['password']
-
-    cursor.execute("SELECT * FROM loginInfo WHERE username = ? AND password = ?", (username, password))
+    # hash pw before comparing
+    hashedpw = generate_sha256_hash(password)
+    cursor.execute("SELECT * FROM loginInfo WHERE username = ? AND password = ?", (username, hashedpw))
     fetched = cursor.fetchall()
 
     conn.commit()
     conn.close()
-
+    # if fetch does not equal [] then that means a record exists in the list
     if fetched != []:
         response = generate_cookie(username)
         #debug_database()
@@ -86,10 +107,12 @@ def login():
     else:
         return render_template('login.html', auth='Login Failed')
 
+# render signup page
 @app.route("/signup.html")
 def signup_page():
     return render_template('signup.html', userexists='')
 
+# handle logic for signing up
 @app.route("/signup", methods=["POST"])
 def signup():
     conn = sqlite3.connect('maindb.db')
@@ -97,15 +120,16 @@ def signup():
 
     username = request.form['username']
     password = request.form['password']
-
+    hashedpw = generate_sha256_hash(password) # hash password
     cursor.execute("SELECT * FROM loginInfo WHERE username = ?", (username,))
-    if (cursor.fetchall() != []):
+    if (cursor.fetchall() != []): # if the list is not empty, the username is alrady in loginInfo
         conn.commit()
         conn.close()
 
         return render_template('signup.html', existsalready='User exists already')
     else:
-        cursor.execute("INSERT INTO loginInfo (username, password) VALUES (?, ?)", (username, password))
+        # insert into loginInfo the username/password
+        cursor.execute("INSERT INTO loginInfo (username, password) VALUES (?, ?)", (username, hashedpw))
 
         conn.commit()
         conn.close()
--- static/chat.js
+++ static/chat.js
@@ -1,45 +1,51 @@
 socket = new WebSocket("ws://localhost:5001");
 
-
 socket.addEventListener('open', function(event) {
+    // this is a special message handled by the server to load initial messages
     socket.send("loadmsg");
 });
 
+// just add notes to the chatdiv
+function update_message_list(message) {
+    const para = document.createElement("p")
+    const node = document.createTextNode(message)
+    para.appendChild(node);
+    const element = document.getElementById("chatdiv");
+    element.appendChild(para);
+    if (element.children.length > 50) {
+        element.children[0].remove();
+    }
+    element.scrollTop = element.scrollHeight;
+}
+
 socket.onmessage = function(event) {
     console.log(event.data)
-    const para = document.createElement("p");
-    let node;
     if (event.data.substring(0, 5) == "init ") {
         initdata = JSON.parse(event.data.replace("init ", ""))
-        console.log(initdata)
+        // loop through json data to add it to div
         for (let i = 0; i < initdata.length; i++) {
-            const para = document.createElement("p");
-            let node = document.createTextNode(initdata[i]["username"] + ": " + initdata[i]["message"]);
-            para.appendChild(node);
-            const element = document.getElementById("chatdiv");
-            element.appendChild(para);
+            let node = initdata[i]["username"] + ": " + initdata[i]["message"];
+            update_message_list(node)
         }
-        return;
     }
+    // update number of people online
     else if (event.data.substring(0, 5) == "peopl") {
-        console.log("ejei")
         value = event.data.substring(12);
         const para = document.getElementById("onlinepeople")
         para.innerHTML = "People online: " + value
     }
     else if (event.data != 'invalidCookieError') {
-    let message = JSON.parse(event.data);
-        node = document.createTextNode(message["username"] + ": " + message["message"]);
+        // add individual message
+        let message = JSON.parse(event.data);
+        update_message_list(message["username"] + ": " + message["message"]);
     }
     else {
-        node = document.createTextNode("An error occurred with authentication. Please try logging in again or clearing your cookies.");
+        // notify user if the cookie is incorrect
+        update_message_list("An error occurred with authentication. Please try logging in again or clearing your cookies.");
     }
-    para.appendChild(node);
-    const element = document.getElementById("chatdiv");
-    element.appendChild(para);
-    element.children[0].remove();
 }; 
 
+// send an individual message to the server
 function send(){
     let authToken = document.cookie.split('=')[1]
     let jsonMessage = {"authToken": authToken, "message": document.getElementById("chatsend").value}
@@ -47,8 +53,18 @@ function send(){
     document.getElementById("chatsend").value = ""
 }
 
+function backToLogin(event) {
+    // reset cookie so user doesnt login immediately again
+    document.cookie = "authCookie=;"
+
+    window.location.href = event.target.href
+}
+
 document.addEventListener("keypress", function onEvent(event) {
     if (event.key === "Enter") {
-        send()
+        let authToken = document.cookie.split('=')[1]
+    let jsonMessage = {"authToken": authToken, "message": document.getElementById("chatsend").value}
+    socket.send(JSON.stringify(jsonMessage));
+    document.getElementById("chatsend").value = ""
     }
 });
